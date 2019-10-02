ae9ef81715a7b99135c5b44c7ef68440f950239d
Amy Huang <ahuang@ucvts.org>
2019-10-01 21:23:51 -0400

    second commit

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -28,22 +28,22 @@ public class ProblemSet2 {
          * name, grade, age, and hometown.
          */
 
-        //prompt
-        System.out.print("Enter your first name: ");
-        String firstName = in.nextLine();
-        System.out.print("Enter your last name: ");
-        String lastName = in.nextLine();
-        System.out.print("Enter your grade: ");
-        int grade = in.nextInt();
-        System.out.print("Enter your age: ");
-        int age = in.nextInt();
-        System.out.print("Enter your hometown:");
-        String hometown = in.nextLine();
-        //results
-        System.out.print("\nNAME      :" + firstName + " " + lastName + "\n");
-        System.out.print("GRADE     :" + grade + "\n");
-        System.out.print("AGE       :" + age + "\n");
-        System.out.print("HOMETOWN  :" + hometown + "\n");
+        // //prompt
+        // System.out.print("Enter your first name: ");
+        // String firstName = in.nextLine();
+        // System.out.print("Enter your last name: ");
+        // String lastName = in.nextLine();
+        // System.out.print("Enter your grade: ");
+        // int grade = in.nextInt();
+        // System.out.print("Enter your age: ");
+        // int age = in.nextInt();
+        // System.out.print("Enter your hometown: ");
+        // String hometown = in.next();
+        // //results
+        // System.out.print("\nNAME      : " + firstName + " " + lastName + "\n");
+        // System.out.print("GRADE     : " + grade + "\n");
+        // System.out.print("AGE       : " + age + "\n");
+        // System.out.print("HOMETOWN  : " + hometown + "\n");
         
         /*
          * Exercise 2.
@@ -51,11 +51,33 @@ public class ProblemSet2 {
          * Given a dollar amount in the range [0.00, 1.00], print the number of dollar
          * bills, quarters, dimes, nickels, and pennies needed to produce this amount.
          */
+        final double DOLLAR = 1.00;
+        final double QUARTER = 0.25;
+        final double DIME = 0.10;
+        final double NICKEL = 0.05;
+        final double PENNY = 0.01;
 
+        System.out.print("Enter a dollar amount: ");
+        double amount = in.nextDouble();
 
+        int dollarAmt = (int)  Math.floor(amount/DOLLAR);
+        double change = amount % DOLLAR;
+        int quarterAmt = (int) Math.floor(change/QUARTER);
+        change %= QUARTER;
+        int dimeAmt = (int) Math.floor(change/DIME);
+        change %= DIME;
+        int nickelAmt = (int) Math.floor(change/NICKEL);
+        change %= NICKEL;
+        int pennyAmt = (int) Math.floor(change/PENNY);
+
+        System.out.println("DOLLARS: " + dollarAmt);
+        System.out.println("QUARTERS: " + quarterAmt);
+        System.out.println("DIMES: " + dimeAmt);
+        System.out.println("NICKElS: " + nickelAmt);
+        System.out.println("PENNY: " + pennyAmt);
 
         /*
-         * Exercise 3.
+         * Exercise 3.)
          * 
          * Given a dollar amount in the range [0.00, 20.00], print the smallest number of
          * bills and coins needed to produce this amount.
